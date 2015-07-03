4249cd0db432a48d8cfeece1445e7614f43f43de
Gexgekko <scgexx@gmail.com>
2015-07-03 21:35:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,15 @@ var recipesApp = angular.module('recipesApp',[]);
 
 recipesApp.controller('RecipesCtrl', ['$scope','$http', function($scope,$http){
 	var vm = $scope;
+	vm.searchItem = Array();
 	$http.get('data/recipes.json').success(function(data){
 		vm.recipeList = data;
 	})
+
+	vm.search = function(ingredient){
+		console.log(vm.searchItem);
+		if(ingredient.recipe == '1'){
+			vm.searchItem.item = ingredient.name;
+		}
+	}
 }])
